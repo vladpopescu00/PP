7e96e43120d61ddf21cfbaeffff99f0891c19245
unknown <Student@C06-12.cs.tuiasi.ro>
2023-02-21 09:37:46 +0200

    Initial commit

---
+++ java-calc/src/Calculator.java
@@ -0,0 +1,108 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+public class Calculator extends JFrame {
+    JButton digits[] = {
+            new JButton(" 0 "),
+            new JButton(" 1 "),
+            new JButton(" 2 "),
+            new JButton(" 3 "),
+            new JButton(" 4 "),
+            new JButton(" 5 "),
+            new JButton(" 6 "),
+            new JButton(" 7 "),
+            new JButton(" 8 "),
+            new JButton(" 9 ")
+    };
+
+    JButton operators[] = {
+            new JButton(" + "),
+            new JButton(" - "),
+            new JButton(" * "),
+            new JButton(" / "),
+            new JButton(" = "),
+            new JButton(" C ")
+    };
+
+    String oper_values[] = {"+", "-", "*", "/", "=", ""};
+
+    String value;
+    char operator;
+
+    JTextArea area = new JTextArea(3, 5);
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.setSize(230, 250);
+        calculator.setTitle(" Java-Calc, PP Lab1 ");
+        calculator.setResizable(false);
+        calculator.setVisible(true);
+        calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public Calculator() {
+        add(new JScrollPane(area), BorderLayout.NORTH);
+        JPanel buttonpanel = new JPanel();
+        buttonpanel.setLayout(new FlowLayout());
+
+        for (int i=0;i<10;i++)
+            buttonpanel.add(digits[i]);
+
+        for (int i=0;i<6;i++)
+            buttonpanel.add(operators[i]);
+
+        add(buttonpanel, BorderLayout.CENTER);
+        area.setForeground(Color.BLACK);
+        area.setBackground(Color.WHITE);
+        area.setLineWrap(true);
+        area.setWrapStyleWord(true);
+        area.setEditable(false);
+
+        for (int i=0;i<10;i++) {
+            int finalI = i;
+            digits[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    area.append(Integer.toString(finalI));
+                }
+            });
+        }
+
+        for (int i=0;i<6;i++){
+            int finalI = i;
+            operators[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    if (finalI == 5)
+                        area.setText("");
+                    else
+                    if (finalI == 4) {
+                        String lhs;
+                        String rhs;
+                        try {
+                            lhs = area.getText().substring(0, area.getText().indexOf(operator + ""));
+                            rhs = area.getText().substring(area.getText().indexOf(operator + "") + 1, area.getText().length());
+                            switch (operator) {
+                                case '+': area.append(" = " + ((Double.parseDouble(lhs) + Double.parseDouble(rhs)))); break;
+                                case '-': area.append(" = " + ((Double.parseDouble(lhs) - Double.parseDouble(rhs)))); break;
+                                case '/': area.append(" = " + ((Double.parseDouble(lhs) / Double.parseDouble(rhs)))); break;
+                                case '*': area.append(" = " + ((Double.parseDouble(lhs) * Double.parseDouble(rhs)))); break;
+                                default: area.setText(" "); break;
+                            }
+                        } catch (Exception e) {
+                            area.setText(" !!!Probleme!!! ");
+                        }
+                    }
+                    else {
+                        area.append(oper_values[finalI]);
+                        operator = oper_values[finalI].charAt(0);
+                    }
+                }
+            });
+        }
+    }
+}
